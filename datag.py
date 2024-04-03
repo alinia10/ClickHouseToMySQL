10c720c1d4667ca6e906a994e8c5c9bc0d0ad117
alinia <ali.niareis@gmail.com>
2024-04-03 12:37:52 +0330

    debug data generator

--- datag.py
+++ datag.py
@@ -6,7 +6,7 @@ import csv
 fake = Faker()
 
 # Define the number of rows
-num_rows = 2000000  # adjust this number to achieve the desired file size
+num_rows = 2_000_000  # adjust this number to achieve the desired file size
 
 # Create a list of dictionaries to hold data
 data = []
@@ -14,9 +14,9 @@ data = []
 # Generate data
 for i in range(num_rows):
     row = {
-        'id': str(i).zfill(12),  # Incremental unique 12-digit ID
+        # 'id': str(i).zfill(12),  # Incremental unique 12-digit ID
         'name': fake.name(),
-        'address': fake.address(),
+        'address': fake.address().replace(',', ' '),
         'email': fake.email(),
         'phone_number': fake.phone_number(),
         'job': fake.job(),
@@ -29,9 +29,9 @@ for i in range(num_rows):
 
 # Create DataFrame
 df = pd.DataFrame(data)
-
+# print(df)
 # Write DataFrame to CSV
-df.to_csv('fake_data.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
+df.to_csv('fake_data.csv', index=True, quoting=csv.QUOTE_NONNUMERIC)
 
 print("CSV file generated successfully.")
 
